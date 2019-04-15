8e54e7033553ce9cfb5e66b424b2b5faaa7eb902
Sara Morell <smorell93@gmail.com>
2019-04-15 16:08:46 -0400

    Updating SI507 Final Project

--- SI507_finalproject.py
+++ SI507_finalproject.py
@@ -38,7 +38,7 @@ class Advertisement(db.Model):
     VideoFile = db.Column(db.String(250))
 
     def __repr__(self):
-        return "{} {}, a 2016 candidate for the U.S. House of Representatives, is running in {} {} against {}".format(self.FirstName,self.LastName,self.State,self.District, self.Opponent)
+        return "{} {}, a 2016 candidate for the U.S. House of Representatives, is running in {} {} against {}.".format(self.FirstName,self.LastName,self.State,self.District, self.Opponent)
 
 class NewInfo(db.Model):
     __tablename__ = 'NewInfo'
@@ -48,39 +48,43 @@ class NewInfo(db.Model):
     Transcript = db.Column(db.String(25000))
 
     def __repr__(self):
-        return "{} {} is a {} candidate".format(TEST.FirstName, TEST.LastName, self.Gender)
+        return "{} {} is a {} candidate".format(self.Ad.FirstName, self.Ad.LastName, self.Gender)
 
-TEST = Advertisement(FirstName = "Sara", LastName = "Morell", State = "NY", District = "12", Opponent = "Nick Paulson", VideoFile = "CAMPAIGNAD")
-db.create_all()
-session.add(TEST)
-session.commit()
-TEST2 = NewInfo(Ad = TEST.id, Gender = "Female", Transcript = "Ad Transcript Would Go Here")
-session.add(TEST2)
-session.commit()
-print(TEST2)
+# TEST = Advertisement(FirstName = "Sara", LastName = "Morell", State = "NY", District = "12", Opp_FirstName = "Nick", Opp_LastName = "Paulson", VideoFile = "CAMPAIGNAD")
+# db.create_all()
+# session.add(TEST)
+# session.commit()
+# TEST2 = NewInfo(Ad = TEST.id, Gender = "Female", Transcript = "Ad Transcript Would Go Here")
+# session.add(TEST2)
+# session.commit()
+# print(TEST2)
+# #
+def random_ad(ad_data):
+    rand_int = random.randint(1, len(ad_data))
+    cand_name = str(ad_data['cand_id'][rand_int])
+    new = cand_name.split(", ")
+    first_name = new[1]
+    last_name = new[0]
+    cand_state = ad_data['state'][rand_int]
+    cand_district = ad_data['district'][rand_int]
+    cand_opponent = str(ad_data['tgt_id'][rand_int])
+    if "," in cand_opponent:
+        oppnew = cand_opponent.split(", ")
+        cand_opponent = str(oppnew[1]) + " " + str(oppnew[0])
+    else:
+        cand_opponent = str(ad_data['tgt_id'][rand_int])
+    ad_title = str(ad_data['vidfile'][rand_int]) + ".wmv"
+    Candidate = Advertisement(FirstName = first_name, LastName = last_name, State = cand_state, District = cand_district, Opponent = cand_opponent, VideoFile = ad_title)
+    return Candidate
+
+#Flask Routes
+@app.route('/campaign_ad/input')
+def video_feed():
+    ad = random_ad(ad_data)
+    session.add(ad)
+    session.commit()
+    return render_template("userinput.html", first_name = ad.FirstName, last_name = ad.LastName, state = ad.State, district = ad.District, cand_opponent = ad.Opponent, video = ad.VideoFile)
 #
-# def random_ad(ad_data):
-#     rand_int = random.randint(1, len(ad_data))
-#     cand_name = str(ad_data['cand_id'][rand_int])
-#     new = cand_name.split(", ")
-#     first_name = new[1]
-#     last_name = new[0]
-#     cand_state = ad_data['state'][rand_int]
-#     cand_district = ad_data['district'][rand_int]
-#     cand_opponent = ad_data['tgt_id'][rand_int]
-#     ad_title = str(ad_data['vidfile'][rand_int]) + ".wmv"
-#     Candidate = Advertisement(FirstName = first_name, LastName = last_name, State = cand_state, District = cand_district, Opponent = cand_opponent, VideoFile = ad_title)
-#     return Candidate
-#
-#
-# #Flask Routes
-# @app.route('/campaign_ad/input')
-# def video_feed():
-#     ad = random_ad(ad_data)
-#     session.add(ad)
-#     session.commit()
-#     return render_template("userinput.html", first_name = ad.FirstName, last_name = ad.LastName, state = ad.State, district = ad.District, Opponent = ad.Opponent, video = ad.VideoFile)
-#
-#if __name__ == '__main__':
-#    db.create_all()
-#     app.run(debug=True)
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
